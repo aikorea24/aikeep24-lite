ef2450f8eebd17eb5bda6158f7b69404a4a6ba50
hugh79757-cmyk <hugh79757@gmail.com>
2026-05-23 01:18:13 +0700

    chore: Worker 설정 config.js 반영 + .bak 파일 정리
    - config.js: WORKER_URL 기본값 설정, loadSettings()에서 ck_worker_url/ck_api_key 로드
    - .gitignore: *.bak 패턴 추가

--- extension/config.js
+++ extension/config.js
@@ -32,6 +32,10 @@ CK.CONFIG = {
   }
 };
 
+// Mode B: Cloudflare Worker 설정
+CK.CONFIG.WORKER_URL = 'https://aikeep24lite-worker.hugh79757.workers.dev';
+CK.CONFIG.API_KEY    = '';  // chrome.storage.local에서 로드
+
 CK.enabled = true;
 CK.isRunning = false;
 CK.lastTurnCount = 0;
@@ -55,6 +59,7 @@ CK.tryParseJSON = function(str) {
 CK.loadSettings = function(callback) {
   var keys = [
     'ck_backend',
+    'ck_worker_url', 'ck_api_key',
     'ck_ollama_model', 'ck_ollama_url', 'ck_worker_url',
     'ck_optiq_url', 'ck_optiq_model',
     'ck_neurons_url', 'ck_neurons_model',
@@ -67,6 +72,8 @@ CK.loadSettings = function(callback) {
     if (data.ck_ollama_url) {
       CK.CONFIG.OLLAMA_TAGS_URL = data.ck_ollama_url + '/api/tags';
     }
+    if (data.ck_worker_url) CK.CONFIG.WORKER_URL = data.ck_worker_url;
+    if (data.ck_api_key)    CK.CONFIG.API_KEY    = data.ck_api_key;
     if (data.ck_turns_per_chunk) CK.CONFIG.TURNS_PER_CHUNK = parseInt(data.ck_turns_per_chunk);
     if (data.ck_max_text_len) CK.CONFIG.MAX_TEXT_LEN = parseInt(data.ck_max_text_len);
     CK.CONFIG.THINKING = data.ck_thinking === 'true';
